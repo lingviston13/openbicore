1a09cea4115ead5aca58f917bee8a38fe456cb20
Open Business <Intelligence>
2013-01-16 18:04:29 +0100

    Licence information added

--- src/org/openbusinessintelligence/tools/file/FileInputBean.java
+++ src/org/openbusinessintelligence/tools/file/FileInputBean.java
@@ -1,3 +1,15 @@
+/*
+Copyright 2013 Open Business Intelligence
+
+This file is part of Foobar.
+
+Open BI Tools is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Open BI Tools is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Open BI Tools. If not, see http://www.gnu.org/licenses/.
+*/
+
 package org.openbusinessintelligence.tools.file;
 
 import java.io.*;
